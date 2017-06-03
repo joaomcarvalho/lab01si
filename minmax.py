7fe2b07b744d0cdbbdb6110a707a9db2dd046ecb
Joao Maurício <joao.carvalho@ccc.ufcg.edu.br>
2017-06-03 19:45:59 -0300

    criando algoritmo de minimo

--- minmax.py
+++ minmax.py
@@ -4,3 +4,10 @@ def maximo(array):
                 if (max < array[i]):
                         max = array[i]
         return max
+
+def minimo(array):
+	min = array[0]
+	for i in range (len(array)):
+		if (min > array[i]):
+			min = array[i]
+	return min
